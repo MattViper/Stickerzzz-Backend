e9f000cf07d6b86317026c51c9f1fa5b5cc9f9c0
Mateusz Żmijewski <mattoshinigami@gmail.com>
2019-10-13 00:34:01 +0200

    fixes, mysql db setup

--- src/Stickerzzz.Infrastructure/StartupSetup.cs
+++ src/Stickerzzz.Infrastructure/StartupSetup.cs
@@ -1,13 +1,21 @@
-﻿using Stickerzzz.Infrastructure.Data;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Pomelo.EntityFrameworkCore.MySql;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Stickerzzz.Infrastructure.Data;
+using System;
 
 namespace Stickerzzz.Infrastructure
 {
 	public static class StartupSetup
 	{
-		public static void AddDbContext(this IServiceCollection services) =>
-			services.AddDbContext<AppDbContext>(options =>
-				options.UseSqlite("Data Source=database.sqlite")); // will be created in web project root
-	}
+        public static void AddDbContext(this IServiceCollection services) =>
+            services.AddDbContextPool<AppDbContext>( // replace "YourDbContext" with the class name of your DbContext
+                options => options.UseMySql("Server=localhost;Database=db;User=root;Password=skrzypak3;", // replace with your Connection String
+                    mySqlOptions =>
+                    {
+                        mySqlOptions.ServerVersion(new Version(8, 0, 17), ServerType.MySql); // replace with your Server Version and Type
+                    }));
+
+    }
 }
