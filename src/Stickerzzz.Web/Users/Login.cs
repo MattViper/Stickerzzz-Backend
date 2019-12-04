940b6e83b487158a428ccc74d555197c462540e7
Mateusz Żmijewski <mattoshinigami@gmail.com>
2019-12-04 22:43:22 +0100

    Login() endpoint, UsersController complete

---
+++ src/Stickerzzz.Web/Users/Login.cs
@@ -0,0 +1,87 @@
+﻿using AutoMapper;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Stickerzzz.Core.Users;
+using Stickerzzz.Infrastructure.Data;
+using Stickerzzz.Infrastructure.Errors;
+using Stickerzzz.Infrastructure.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Stickerzzz.Web.Users
+{
+    public class Login
+    {
+        public class UserData
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class UserDataValidator : AbstractValidator<UserData>
+        {
+            public UserDataValidator()
+            {
+                RuleFor(x => x.Email).NotNull().NotEmpty();
+                RuleFor(x => x.Password).NotNull().NotEmpty();
+            }
+        }
+
+        public class Command : IRequest<UserEnvelope>
+        {
+            public UserData User { get; set; }
+        }
+
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.User).NotNull().SetValidator(new UserDataValidator());
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, UserEnvelope>
+        {
+            private readonly AppDbContext _context;
+            private readonly IPasswordHasher _passwordHasher;
+            private readonly IJwtTokenGenerator _jwtTokenGenerator;
+            private readonly IMapper _mapper;
+
+            public Handler(
+                AppDbContext context,
+                IPasswordHasher passwordHasher,
+                IJwtTokenGenerator jwtTokenGenerator,
+                IMapper mapper)
+            {
+                _context = context;
+                _passwordHasher = passwordHasher;
+                _jwtTokenGenerator = jwtTokenGenerator;
+                _mapper = mapper;
+            }
+
+            public async Task<UserEnvelope> Handle(Command message, CancellationToken cancellationToken)
+            {
+                var appUser = await _context.Users.Where(i => i.Email == message.User.Email).SingleOrDefaultAsync(cancellationToken);
+                if (appUser == null)
+                {
+                    throw new RestException(HttpStatusCode.Unauthorized, new { Error = "Invalid email / password." });
+                }
+
+                if (!appUser.Hash.SequenceEqual(_passwordHasher.Hash(message.User.Password, appUser.Salt)))
+                {
+                    throw new RestException(HttpStatusCode.Unauthorized, new { Error = "Invalid email / password." });
+                }
+
+                var user = _mapper.Map<AppUser, User>(appUser);
+                user.Token = await _jwtTokenGenerator.CreateToken(appUser.UserName);
+
+                return new UserEnvelope(user);
+            }
+        }
+    }
+}
---
+++ src/Stickerzzz.Web/Users/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stickerzzz.Web.Users
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController
+    {
+        private readonly IMediator _mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<UserEnvelope> Create([FromBody] Create.Command command) => await _mediator.Send(command);
+
+        [HttpPost]
+        public async Task<UserEnvelope> Login([FromBody] Login.Command command) => await _mediator.Send(command);
+    }
+}
