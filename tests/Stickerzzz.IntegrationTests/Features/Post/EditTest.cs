e0f949ac07bb6a429ce7e14970c70f06b22e1aa4
MMmmMMmm313@wp.pl <MMmmMMmm313@wp.pl>
2019-12-05 17:53:49 +0100

    edit test

---
+++ tests/Stickerzzz.IntegrationTests/Features/Post/EditTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Stickerzzz.IntegrationTests.Features.Post
+{
+    public class EditTest : Slicefixture
+    {
+        [Fact]
+        public async Task Expect_Edit_Post()
+        {
+            var createCommand = new Create.Command()
+            {
+                Post = new Create.PostData()
+                {
+                    Title = "Test post",
+                    Description = "Description of the test post",
+                    Body = "Body of the test post",
+                    TagList = new string[] { "tag1", "tag2" }
+                }
+
+            };
+        }
+    }
+}
