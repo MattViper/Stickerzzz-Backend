eef1e98cc83b3b1026763bf217ad7f014e291e68
MMmmMMmm313@wp.pl <MMmmMMmm313@wp.pl>
2019-12-05 18:16:48 +0100

    post helper

--- tests/Stickerzzz.IntegrationTests/Features/Post/PostHelpers.cs
+++ tests/Stickerzzz.IntegrationTests/Features/Post/PostHelpers.cs
@@ -10,43 +10,25 @@ namespace Stickerzzz.IntegrationTests.Features.Post
     class PostHelpers
     {
         // <summary>
-
         // creates an post based on the given Create command.It also creates a default user
-
         // </summary>
-
         // <param name = "fixture" ></ param >
-
         // < param name="command"></param>
-
         // <returns></returns>
 
         public static async Task<Stickerzzz.Core.Entities.Post> CreateArticle(SliceFixture fixture, Create.Command command)
-
         {
-
             //first create the default user
-
            var user = await UserHelpers.CreateDefaultUser(fixture);
 
-
-
             var dbContext = fixture.GetDbContext();
-
             var currentAccessor = new StubCurrentUserAccessor(user.Username);
 
-
-
             var postCreateHandler = new Create.Handler(dbContext, currentAccessor);
-
             var created = await postCreateHandler.Handle(command, new System.Threading.CancellationToken());
 
-
             var dbPost = await fixture.ExecuteDbContextAsync(db => db.Posts.Where(a => a.Id == created.Post.Id)
-
                 .SingleOrDefaultAsync());
-
-
 
             return dbPost;
         }
