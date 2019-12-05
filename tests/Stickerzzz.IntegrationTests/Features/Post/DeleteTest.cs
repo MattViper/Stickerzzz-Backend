392d29fbbb6d5dd8b8456aaa347648b37ff1f742
MMmmMMmm313@wp.pl <MMmmMMmm313@wp.pl>
2019-12-05 17:15:42 +0100

    test

--- tests/Stickerzzz.IntegrationTests/Features/Post/DeleteTest.cs
+++ tests/Stickerzzz.IntegrationTests/Features/Post/DeleteTest.cs
@@ -37,6 +37,65 @@ namespace Stickerzzz.IntegrationTests.Features.Post
             Assert.Null(dbPost);
         }
 
-       
-    }
+        [Fact]
+        public async Task Expect_Delete_Post_With_Tags()
+        {
+            var createCmd = new Create.Command()
+
+            {
+                Post = new Create.PostData()
+                {
+                    Title = "Test post",
+                    Description = "Description of the test post",
+                    Body = "Body of the test post",
+                    TagList = new string[] { "tag1", "tag2" }
+                }
+            };
+            var post = await PostHelpers.CreatePost(this, createCmd);
+
+            var dbPostWithTags = await ExecuteDbContextAsync(
+
+                db => db.Post.Include(a => a.PostTags)
+
+                .Where(d => d.Slug == post.Slug).SingleOrDefaultAsync()
+
+            );
+
+
+            var deleteCmd = new Delete.Command(post.Slug);
+
+            var dbContext = GetDbContext();
+
+            var postDeleteHandler = new Delete.QueryHandler(dbContext);
+            await postDeleteHandler.Handle(deleteCmd, new System.Threading.CancellationToken());
+
+            var dbPost = await ExecuteDbContextAsync(db => db.Post.Where(d => d.Slug == deleteCmd.Slug).SingleOrDefaultAsync());
+            Assert.Null(dbPost);
+
+        }
+        [Fact]
+
+        public async Task Expect_Delete_Post_With_Comments()
+
+        {
+            var createPostCmd = new Create.Command()
+            {
+                Post = new Create.PostData()
+                {
+                    Title = "Test post",
+                    Description = "Description of the test post",
+                    Body = "Body of the test post",
+                }
+            };
+
+
+            var post = await PostHelpers.CreatePost(this, createPostCmd);
+            var dbPost = await ExecuteDbContextAsync(
+                db => db.Post.Include(a => a.PostTags)
+                .Where(d => d.Slug == Post.Slug).SingleOrDefaultAsync()
+            );
+            var Id = dbPost.Id;
+            var slug = dbPost.Slug;
+        }
+        }
 }
