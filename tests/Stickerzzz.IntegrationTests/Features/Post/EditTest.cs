3781598c9bc879890b5986e0903363969c70b9d1
MMmmMMmm313@wp.pl <MMmmMMmm313@wp.pl>
2019-12-05 18:00:27 +0100

    edit test

--- tests/Stickerzzz.IntegrationTests/Features/Post/EditTest.cs
+++ tests/Stickerzzz.IntegrationTests/Features/Post/EditTest.cs
@@ -31,6 +31,21 @@ namespace Stickerzzz.IntegrationTests.Features.Post
                 },
                 Slug = createdPost.Slug
             };
+            // remove the first tag and add a new tag
+
+            command.Post.TagList = new string[] { createdPost.TagList[1], "tag3" };
+
+            var dbContext = GetDbContext();
+
+            var postEditHandler = new Edit.Handler(dbContext);
+            var edited = await postEditHandler.Handle(command, new System.Threading.CancellationToken());
+
+            Assert.NotNull(edited);
+            Assert.Equal(edited.Post.Title, command.Post.Title);
+            Assert.Equal(edited.Post.TagList.Count(), command.Post.TagList.Count());
+            // use assert Contains because we do not know the order in which the tags are saved/retrieved
+            Assert.Contains(edited.Post.TagList[0], command.Post.TagList);
+            Assert.Contains(edited.Post.TagList[1], command.Post.TagList);
         }
     }
 }
